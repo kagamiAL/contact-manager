b8ee2f4a0f74682c40b598907d653361f83ce3e9
Alan Bach <kagamibach@gmail.com>
2025-01-11 21:50:59 -0700

    Add creation of auth provider

--- src/main/java/org/alan/contact_manager_backend/config/WebSecurityConfig.java
+++ src/main/java/org/alan/contact_manager_backend/config/WebSecurityConfig.java
@@ -1,15 +1,19 @@
 package org.alan.contact_manager_backend.config;
 
 import org.alan.contact_manager_backend.models.UserRoles;
+import org.alan.contact_manager_backend.services.AppUserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -19,6 +23,22 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class WebSecurityConfig {
+
+    private final AppUserService appUserService;
+    private final PasswordEncoder passwordEncoder;
+
+    public WebSecurityConfig(AppUserService appUserService, PasswordEncoder passwordEncoder) {
+        this.appUserService = appUserService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(appUserService.userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return authenticationProvider;
+    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
