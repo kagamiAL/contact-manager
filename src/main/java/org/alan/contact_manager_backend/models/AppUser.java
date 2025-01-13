5253f665843090d6b85ed82b8e72658ffa3d1e3d
Alan Bach <kagamibach@gmail.com>
2025-01-12 22:14:08 -0700

    Emails must be unique

--- src/main/java/org/alan/contact_manager_backend/models/AppUser.java
+++ src/main/java/org/alan/contact_manager_backend/models/AppUser.java
@@ -15,7 +15,7 @@ public class AppUser implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
