f779676b9f180c45268dc4031b7cd0128ccd3b44
Alan Bach <kagamibach@gmail.com>
2025-06-25 13:10:49 -0600

    Batch check duplicates

--- contact_manager_backend/src/main/java/org/alan/contact_manager_backend/repositories/ContactRepository.java
+++ contact_manager_backend/src/main/java/org/alan/contact_manager_backend/repositories/ContactRepository.java
@@ -5,24 +5,13 @@ import org.alan.contact_manager_backend.models.Contact;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
 import java.util.Collection;
-import java.util.Optional;
 
 public interface ContactRepository extends JpaRepository<Contact, Long> {
     void deleteAllByAppUserAndIdIn(AppUser appUser, Collection<Long> ids);
 
     Page<Contact> findAllByAppUser(AppUser appUser, Pageable pageable);
 
-    @Query("SELECT c FROM Contact c WHERE c.appUser = :appUser AND c.firstName = :firstName AND c.lastName = :lastName AND c.zipCode = :zipCode AND c.dateOfBirth = :dateOfBirth")
-    Optional<Contact> findDuplicateContact(
-            @Param("appUser") AppUser appUser,
-            @Param("firstName") String firstName,
-            @Param("lastName") String lastName,
-            @Param("zipCode") String zipCode,
-            @Param("dateOfBirth") LocalDate dateOfBirth
-    );
+    Collection<Contact> findAllByAppUserAndUniqueHashIn(AppUser appUser, Collection<String> uniqueHashes);
 }
