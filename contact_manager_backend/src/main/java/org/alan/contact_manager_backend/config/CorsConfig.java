7e8113d07258baa634c164a6802bdf99c59fd649
Alan Bach <kagamibach@gmail.com>
2025-08-04 20:22:59 -0600

    Should generalize this stuff

--- contact_manager_backend/src/main/java/org/alan/contact_manager_backend/config/CorsConfig.java
+++ contact_manager_backend/src/main/java/org/alan/contact_manager_backend/config/CorsConfig.java
@@ -1,19 +1,22 @@
 package org.alan.contact_manager_backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
-
 @Configuration
 public class CorsConfig {
+
+    @Value("${FRONTEND_URL}")
+    String frontEndURL;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.addAllowedOrigin(frontEndURL);
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
